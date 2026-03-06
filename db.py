f32327f32a7f1fa0aa44b0f02514a0380367d9c6
akreatharv4-cpu <akreatharv4@gmail.com>
2026-03-06 12:50:03 +0530

    Update db.py

--- db.py
+++ db.py
@@ -30,10 +30,3 @@ def get_db() -> Generator:
         yield db
     finally:
         db.close()
-from flask import Flask, send_from_directory
-
-app = Flask(__name__, static_folder=".")
-
-@app.route("/")
-def home():
-    return send_from_directory(".", "index.html")
