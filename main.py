388806e5c9d6d6c04ba76010fa88953057ed8aa3
akreatharv4-cpu <akreatharv4@gmail.com>
2026-03-06 12:50:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,3 +49,10 @@ if __name__ == "__main__":
     app.run()
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder=".")
+
+@app.route("/")
+def home():
+    return send_from_directory(".", "index.html")
