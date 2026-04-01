98f87d6b099f3c459263f598ba52d07c051c1d49
akreatharv4-cpu <akreatharv4@gmail.com>
2026-04-01 13:50:59 +0530

    Update extractor.py

--- bert_module/extractor.py
+++ bert_module/extractor.py
@@ -1,1 +1,28 @@
+from bert_module.biobert import extract_medical_entities
 
+
+def extract_drug_names(text):
+    entities = extract_medical_entities(text)
+
+    drugs = []
+
+    for e in entities:
+        word = e["word"]
+
+        # Basic cleaning (remove ## from tokens)
+        if word.startswith("##"):
+            if drugs:
+                drugs[-1] += word[2:]
+        else:
+            drugs.append(word)
+
+    return drugs
+
+
+def extract_clean_drugs(text):
+    raw_drugs = extract_drug_names(text)
+
+    # Remove duplicates and clean
+    clean = list(set([d.strip() for d in raw_drugs if len(d) > 2]))
+
+    return clean
