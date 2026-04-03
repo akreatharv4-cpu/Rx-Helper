f27c7b587c1402c0929349ea15cd797aff39a1bc
akreatharv4-cpu <akreatharv4@gmail.com>
2026-04-03 10:40:57 +0530

    Update pipeline.py

--- bert_module/app/services/pipeline.py
+++ bert_module/app/services/pipeline.py
@@ -1,1 +1,22 @@
+from app.utils.text_cleaner import clean_text
+from app.utils.drug_matcher import find_drugs
+from app.utils.biobert_ner import extract_medical_entities
 
+def process_prescription(raw_text: str):
+    
+    # Step 1: Clean
+    cleaned_text = clean_text(raw_text)
+    
+    # Step 2: Fuzzy
+    fuzzy_drugs = find_drugs(cleaned_text)
+    
+    # Step 3: BioBERT
+    bert_drugs = extract_medical_entities(raw_text)
+    
+    # Step 4: Combine
+    final_drugs = list(set(fuzzy_drugs + bert_drugs))
+    
+    return {
+        "cleaned_text": cleaned_text,
+        "detected_drugs": final_drugs
+    }
